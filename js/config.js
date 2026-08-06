210aab93ec7a25bd5fb94dd2ffbb161b3679e79d
SST-maker <nacerito83@gmail.com>
2026-08-07 00:31:06 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Configuration publique Supabase de l’agenda familial.
-export const SUPABASE_URL = 'https://myvnrhyoypjkhkmdokqf.supabase.coo';
+export const SUPABASE_URL = 'https://myvnrhyoypjkhkmdokqf.supabase.co';
 export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_Iu24aI5cvpmD14l9qhMnZQ_48c1560X';
